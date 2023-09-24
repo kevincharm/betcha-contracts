84af0f6b140d83053d04de32295c5ba60491c808
Kevin Charm <kevin@kevincharm.com>
2023-09-23 23:40:57 -0400

    implement contract verification

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,33 @@
-import { ethers } from 'hardhat'
-import { BetchaRoundFactory__factory } from '../typechain-types'
+import { ethers, run } from 'hardhat'
+import { BetchaRoundFactory__factory, BetchaRound__factory } from '../typechain-types'
 import { BASE_GNOSIS_SAFE_MASTERCOPY, BASE_GNOSIS_SAFE_PROXY_FACTORY } from '../lib/config'
 
 async function main() {
     const [deployer] = await ethers.getSigners()
-    const factory = await new BetchaRoundFactory__factory(deployer).deploy(
-        BASE_GNOSIS_SAFE_PROXY_FACTORY,
-        BASE_GNOSIS_SAFE_MASTERCOPY,
-    )
-    await factory.waitForDeployment()
+    const masterCopy = await new BetchaRound__factory(deployer)
+        .deploy()
+        .then((contract) => contract.waitForDeployment())
+    const factoryConstructorArgs: Parameters<
+        InstanceType<typeof BetchaRoundFactory__factory>['deploy']
+    > = [BASE_GNOSIS_SAFE_PROXY_FACTORY, BASE_GNOSIS_SAFE_MASTERCOPY, await masterCopy.getAddress()]
+    const factory = await new BetchaRoundFactory__factory(deployer)
+        .deploy(...factoryConstructorArgs)
+        .then((contract) => contract.waitForDeployment())
     console.log(`Factory deployed to: ${await factory.getAddress()}`)
+
+    // Wait for etherscan to catch up
+    await new Promise((resolve) => setTimeout(resolve, 60_000))
+
+    // Verify mastercopy
+    await run('verify:verify', {
+        address: await masterCopy.getAddress(),
+        constructorArguments: [],
+    })
+    // Verify factory
+    await run('verify:verify', {
+        address: await factory.getAddress(),
+        constructorArguments: factoryConstructorArgs,
+    })
 }
 
 main().catch((error) => {
